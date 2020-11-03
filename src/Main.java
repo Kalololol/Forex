b46863a0feaca2cec65c67fd8480c30ddb1fbb5f
Kalololol <61520276+Kalololol@users.noreply.github.com>
2020-11-03 18:26:24 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
+import logic.*;
 import view.MainMenu;
+
+import javax.swing.*;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class Main {
     public static  void main(String[] args) {
-        MainMenu menu = new MainMenu();
-        menu.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        menu.setVisible(true);
+//        MainMenu menu = new MainMenu();
+//        menu.setDefaultCloseOperation(EXIT_ON_CLOSE);
+//        menu.setVisible(true);
+        Menu start = new Menu();
+
+
+
     }
 }

748f1c445903d922e7a99da950192d6c27895c89
Kalololol <61520276+Kalololol@users.noreply.github.com>
2020-11-11 21:55:54 +0100

    Update Transaction.java

--- src/logic/Transaction.java
+++ src/logic/Transaction.java
@@ -1,46 +1,28 @@
 package logic;
 
 
-import java.util.Date;
-import java.util.Scanner;
-
 public class Transaction {
     private double balance;
     private String date;
 
-    public Transaction (){
-        setDate();
-        setBalance();
-
-    }
-
-    Scanner scanner = new Scanner(System.in);
-
-    @Override
-    public String toString() {
-        return "Transaction{" +
-                "balance=" + balance +
-                ", date='" + date + '\'' +
-                '}';
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate() {
-        System.out.println("Data: (w formacie dd.mm): ");
-        date = scanner.nextLine();
+    public Transaction(double balance, String date) {
+        this.balance = balance;
+        this.date = date;
     }
 
     public double getBalance() {
         return balance;
     }
 
-    public void setBalance() {
-        System.out.println("Wartość: ");
-        balance = scanner.nextDouble();
-        scanner.nextLine();
+    public void setBalance(double balance) {
+        this.balance = balance;
     }
 
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
